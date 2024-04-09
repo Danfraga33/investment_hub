0d5adbf9e9d12fe76bab30aab4cbcb77eb415aa9
Daniel Fraga <dan.fraga33@gmail.com>
2024-04-09 16:45:48 +0100

    fix: fix import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "./lib/utils";
+import { cn } from "../lib/utils";
 import Navbar from "./components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 
