bc5cd386a500f5179480d672546ba5f01f56145c
Daniel Fraga <dan.fraga33@gmail.com>
2024-04-11 08:55:28 +0100

    fix: delete auth callback and middleware file

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: ["/", "/pricing"],
-});
-
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
