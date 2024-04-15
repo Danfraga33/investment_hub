4f6b639bfd0fe6f66fb0d03c55a3bb40c15c5203
Daniel Fraga <dan.fraga33@gmail.com>
2024-04-15 22:51:41 +0100

    feat: db accelerate

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -1,11 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 // This is to mitigate the error message: Prisma client is initialized constantly (>10 times)
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// export const prisma =
+//   globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate());
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export const db = prisma;
+export const db = new PrismaClient().$extends(withAccelerate());
