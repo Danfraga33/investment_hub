5321c9191a3ae1056446902ff5cbcfa9f8c8f4ec
Daniel Fraga <dan.fraga33@gmail.com>
2024-04-15 20:04:08 +0100

    feat: protect dashboard path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,19 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { auth } from "@/lib/auth";
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/login", request.url));
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+  if (!session) {
+    return NextResponse.redirect(new URL("/api/auth/signin", request.url));
+  }
 }
 
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-    "/dashboard/:path*",
-  ],
+  matcher: ["/dashboard/:path*"],
 };
+
+/**
+ * 2. Prisma Adapter inclusion, renders the entire login poge disabled
+ * 3. After signout URL
+ */
