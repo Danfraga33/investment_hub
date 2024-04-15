c97826d0250cfd18881f609c8385c243a99b6cd8
Daniel Fraga <dan.fraga33@gmail.com>
2024-04-15 20:05:04 +0100

    feat: prisma adapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { db } from "./db";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(db),
   providers: [Google],
 });
