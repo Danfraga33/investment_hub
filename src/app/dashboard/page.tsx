1a1956622a95085e2f74a0e536e98b8ecb671cce
Daniel Fraga <dan.fraga33@gmail.com>
2024-04-18 00:25:37 +0100

    style: adusting layout screen size

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,10 @@ import Dashboard from "../components/Dashboard";
 
 const Page = () => {
   return (
-    <div className="mt-3">
+    <div
+      className="mt-3 px-3 h-screen "
+      style={{ height: "calc(100vh - 5rem)" }}
+    >
       <Dashboard />;
     </div>
   );
