3050faedbc7ab363a1eb8ab9185ee32048c75897
Daniel Fraga <dan.fraga33@gmail.com>
2024-04-30 08:21:41 +0100

    feat: add SSR feature

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,51 @@
+import { Prisma } from "@prisma/client";
 import Dashboard from "../components/Dashboard";
+import { DashboardData } from "../components/DashboardData";
+export type PortfolioWithStocks = Prisma.PortfolioGetPayload<{
+  include: { stocks: true };
+}>;
 
-const Page = () => {
+const Page = async () => {
+  const res = await DashboardData();
+  const collectionOfPortfolios: PortfolioWithStocks[] = await res.json();
+
+  // function AddToCurrentPortfolio(symbol: string, name: string) {
+  //   /**
+  //    * Add to stocks through a post req
+  //    * Saved in same portfolio
+  //    */
+  //   console.log({ symbol, name });
+  //   const addStock = async () => {
+  //     try {
+  //       const response = await fetch("/api/portfolioData", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({ portfolioId, symbol, name }),
+  //       });
+
+  //       console.log("Adding to portfolio:", portfolioId, name);
+  //       if (!response.ok) {
+  //         throw new Error("Failed to add stock to portfolio.");
+  //       }
+
+  //       console.log("Successfully added stock.");
+  //       // setPortfolio((prev) => [...prev, response.body]);
+  //     } catch (error) {
+  //       console.error("Error adding stock to portfolio:", error);
+  //     }
+  //   };
+
+  //   addStock();
+  // }
+
   return (
     <div
       className="mt-3 px-3 h-screen overflow-hidden "
       style={{ height: "calc(100vh - 5rem)" }}
     >
-      <Dashboard />;
+      <Dashboard collectionOfPortfolios={collectionOfPortfolios} />;
     </div>
   );
 };
