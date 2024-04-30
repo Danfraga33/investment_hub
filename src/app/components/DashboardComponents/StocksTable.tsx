f3008ba0ab824309028bb91cade890d7c83188e4
Daniel Fraga <dan.fraga33@gmail.com>
2024-04-30 08:23:33 +0100

    fix: remove uneccessary code

--- src/app/components/DashboardComponents/StocksTable.tsx
+++ src/app/components/DashboardComponents/StocksTable.tsx
@@ -2,30 +2,26 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { ComboboxDemo } from "./Combobox";
-import { Prisma } from "@prisma/client";
-type PortfolioWithStocks = Prisma.PortfolioGetPayload<{
-  include: { stocks: true };
-}>;
+import { PortfolioWithStocks } from "@/app/dashboard/page";
 
-type AddToCurrentPortfolio = (portfolioId: string, name: string) => void;
-export function StocksTable({
-  portfolio,
-  AddToCurrentPortfolio,
-}: {
-  portfolio: PortfolioWithStocks;
-  AddToCurrentPortfolio: AddToCurrentPortfolio;
-}) {
+export type Portfolio = {
+  selectedPortfolio: PortfolioWithStocks;
+};
+
+export function StocksTable({ selectedPortfolio }: Portfolio) {
+  console.log(selectedPortfolio);
+  const stocks = selectedPortfolio.stocks;
   return (
     <ScrollArea className="h-full w-full rounded-md border p-2">
       <div className="mb-4 px-2 py-1 flex items-center justify-between">
         <h4 className="text-md font-semibold leading-none">Companies</h4>
         <ComboboxDemo
-          portfolio={portfolio}
-          AddToCurrentPortfolio={AddToCurrentPortfolio}
+          selectedPortfolio={selectedPortfolio}
+          // AddToCurrentPortfolio={AddToCurrentPortfolio}
         />
       </div>
 
-      {portfolio.stocks.map((company) => (
+      {stocks.map((company) => (
         <div key={company.id}>
           <Button
             className="w-full items-start text-start "
