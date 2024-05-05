6f20e5f1f67e26713ad399d32935d7d0b7c2e61f
Daniel Fraga <dan.fraga33@gmail.com>
2024-05-05 23:42:18 +0100

    feat: make breadcrumb comp dynamic

--- src/app/components/DashboardComponents/Block/Breadcrumbs.tsx
+++ src/app/components/DashboardComponents/Block/Breadcrumbs.tsx
@@ -7,26 +7,25 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export const Breadcrumbs = () => {
+  const pathname = usePathname(); // Dashboard/Analytics
+  const Base = pathname.split("/").filter((segment) => segment.length > 0)[0];
+  const Path = pathname.split("/").filter((segment) => segment.length > 0)[1];
+
   return (
     <Breadcrumb className="hidden md:flex">
       <BreadcrumbList>
         <BreadcrumbItem>
           <BreadcrumbLink asChild>
-            <Link href="#">Dashboard</Link>
+            <Link href="/Dashboard">{Base}</Link>
           </BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbSeparator />
         <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            <Link href="#">Portfolio</Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Insights</BreadcrumbPage>
+          <BreadcrumbPage>{Path}</BreadcrumbPage>
         </BreadcrumbItem>
       </BreadcrumbList>
     </Breadcrumb>
