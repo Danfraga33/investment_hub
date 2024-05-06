aa398bff89471d29ab95b22f7fc5e59d9294bafa
Daniel Fraga <dan.fraga33@gmail.com>
2024-05-06 20:01:29 +0100

    chore: remove unneeded imports

--- src/app/components/DashboardComponents/UserNav.tsx
+++ src/app/components/DashboardComponents/UserNav.tsx
@@ -8,7 +8,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { signOut, useSession } from "next-auth/react";
 export function UserNav() {
   const { data } = useSession();
@@ -18,7 +18,7 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            {/* <AvatarImage src="/avatars/01.png" alt="@shadcn" /> */}
             <AvatarFallback>D</AvatarFallback>
           </Avatar>
         </Button>
