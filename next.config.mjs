404625c3d67b92c4c00e1dbad4ef1ed35bd1f68b
Daniel Fraga <dan.fraga33@gmail.com>
2024-05-10 21:22:45 +0100

    chore: include image paths for image next use

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,86 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static2.finnhub.io",
+        port: "",
+        pathname: "/file/publicdatany/finnhubimage/stock_logo/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static.finnhub.io",
+        port: "",
+        pathname: "/logo/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s.yimg.com",
+        port: "",
+        pathname: "/ny/api/res/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static.seekingalpha.com",
+        port: "",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s.yimg.com",
+        port: "",
+        pathname: "/cv/apiv2/social/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.zenfs.com",
+        port: "",
+        pathname: "/en/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.telegraph.co.uk",
+        port: "",
+        pathname: "/content/dam/business/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static.seekingalpha.com",
+        port: "",
+        pathname: "/cdn/s3/uploads/getty_images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "static.standard.co.uk",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "g.foolcdn.com",
+        port: "",
+        pathname: "/editorial/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s.yimg.com",
+        port: "",
+        pathname: "/ny/api/res/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s.yimg.com",
+        port: "",
+        pathname: "/ny/api/res/1.2/**",
+      },
+    ],
+  },
+};
 export default nextConfig;
