38f4981a8b0822ad4a4820317e13bc74cd35c6d4
Daniel Fraga <dan.fraga33@gmail.com>
2024-05-23 14:13:04 +0200

    fix: change text on landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,15 @@ export default function Home() {
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semi-bold text-gray-700">
-            Financial Metrics Visualizer
+            Simplify Financial Analysis
           </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Visualize Your <span className="text-green-500">Financial Data</span>
+          Unlock <span className="text-green-500">Insights</span> in Seconds
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Upload your company &apos;s financial statements and uncover key
-          insights with interactive charts and graphs.
+          Unlock Higher Returns: Seamlessly Access Individual Investments for
+          Informed Decision-Making and Financial Growth
         </p>
 
         <Link
