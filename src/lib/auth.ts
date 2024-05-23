b0a0b9cd281328f47414c8d3421f00eab5d03bb1
Daniel Fraga <dan.fraga33@gmail.com>
2024-05-23 14:08:23 +0200

    fix: edit auth settings for google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,23 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { db } from "./db";
+import type { Provider } from "next-auth/providers";
+
+const providers: Provider[] = [Google];
+
+export const providerMap = providers.map((provider) => {
+  if (typeof provider === "function") {
+    const providerData = provider();
+    return { id: providerData.id, name: providerData.name };
+  } else {
+    return { id: provider.id, name: provider.name };
+  }
+});
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
-  providers: [Google],
+  providers,
+  pages: {
+    signIn: "/signin",
+  },
 });
