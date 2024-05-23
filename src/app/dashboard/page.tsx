2c9d284d03a375f66290ad1867f8c609415d0216
Daniel Fraga <dan.fraga33@gmail.com>
2024-05-23 15:51:57 +0200

    fix: dashboard page layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,6 +9,7 @@ import {
   MoveUpRight,
   MoveVerticalIcon,
   PieChart,
+  Sparkles,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
@@ -23,28 +24,31 @@ import { cn } from "@/lib/utils";
 
 const Holdings = async () => {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen  flex flex-col">
       <MaxWidthWrapper>
-        <section className="bg-blue-700">
+        <section className="bg-white">
           <section className="rounded-xl p-6 shadow-md sm:gap-4 sm:py-4 sm:pl-14 px-4">
             <div className="flex justify-between items-start mb-6">
-              <div className="flex gap-2">
-                <h2 className="text-3xl font-bold text-[#14142B]">
-                  Total Balance: $36,254
-                </h2>
-                <Badge className={cn("", "bg-green-300")}>
-                  <MoveUpRight size={8} />
-                </Badge>
-
-                <p className="text-sm text-[#6E7191] mt-1">
-                  +4.72% Daily Change
-                </p>
+              <div className="flex flex-col gap-2 py-6">
+                <p className="text-xs">Total Balance</p>
+                <div className="flex gap-4 items-end">
+                  <h2 className="text-4xl font-semibold text-[#14142B]">
+                    $36,254
+                  </h2>
+                  <div className="flex items-end gap-2">
+                    <Badge className="bg-green-300 px-1 py-1">
+                      <p className="text-xs p-0 m-0">+4.72%</p>
+                    </Badge>
+                    <p className="text-neutral-500/55 text-xs ">
+                      Since Yesterday
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className="flex space-x-2">
-                <Button variant="ghost">View Analytics</Button>
-                <Button variant="secondary">Send Money</Button>
-                <Button variant="secondary">Add Money</Button>
-              </div>
+            </div>
+            <div className="flex items-center gap-2 pb-4">
+              <Sparkles />
+              <p className="text-xl tracking-tight">Overview</p>
             </div>
             <div className="grid grid-cols-3 gap-6 mb-8">
               <Card className="bg-[#EFF0F7] p-4 rounded-lg">
@@ -82,22 +86,21 @@ const Holdings = async () => {
                 </div>
               </Card>
             </div>
-          </section>
-        </section>
-        <section>
-          <div className="flex justify-between items-center mb-4">
-            <h3 className="text-lg font-semibold text-[#14142B]">
-              Recent Transactions
-            </h3>
-            <Link
-              className="text-sm text-[#6E7191] hover:text-[#14142B]"
-              href="#"
-            >
-              See All
-            </Link>
-          </div>
-          <Table>
-            {/* <TableHeader>
+
+            <section>
+              <div className="flex justify-between items-center mb-4">
+                <h3 className="text-lg font-semibold text-[#14142B]">
+                  Recent Transactions
+                </h3>
+                <Link
+                  className="text-sm text-[#6E7191] hover:text-[#14142B]"
+                  href="#"
+                >
+                  See All
+                </Link>
+              </div>
+              <Table>
+                {/* <TableHeader>
               <TableRow>
                 <TableHead className="w-[100px]">Invoice</TableHead>
                 <TableHead>Status</TableHead>
@@ -105,39 +108,31 @@ const Holdings = async () => {
                 <TableHead className="text-right">Amount</TableHead>
               </TableRow>
             </TableHeader> */}
-            <TableBody>
-              <TableRow>
-                <TableCell className="font-medium">
-                  <div className="flex items-center space-x-2">
-                    <CreditCardIcon className="text-[#6E7191] h-6 w-6" />
-                    <div>
-                      <p className="text-sm font-medium text-[#14142B]">AAPL</p>
-                      <p className="text-xs text-[#6E7191]">Name</p>
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell>Technology</TableCell>
-                <TableCell>+3.45%</TableCell>
+                <TableBody>
+                  <TableRow>
+                    <TableCell className="font-medium">
+                      <div className="flex items-center space-x-2">
+                        <CreditCardIcon className="text-[#6E7191] h-6 w-6" />
+                        <div>
+                          <p className="text-sm font-medium text-[#14142B]">
+                            AAPL
+                          </p>
+                          <p className="text-xs text-[#6E7191]">Apple Inc.</p>
+                        </div>
+                      </div>
+                    </TableCell>
+                    <TableCell>Technology</TableCell>
+                    <TableCell className="text-right">+3.45%</TableCell>
 
-                <TableCell className="text-right">$4.41</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium">
-                  <div className="flex items-center space-x-2">
-                    <CreditCardIcon className="text-[#6E7191] h-6 w-6" />
-                    <div>
-                      <p className="text-sm font-medium text-[#14142B]">AAPL</p>
-                      <p className="text-xs text-[#6E7191]">Name</p>
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell>Technology</TableCell>
-                <TableCell>+3.45%</TableCell>
-
-                <TableCell className="text-right">$4.41</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
+                    <TableCell className="text-right">$4.41</TableCell>
+                    <TableCell className="text-right">
+                      <Button variant="outline">Analyze</Button>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </section>
+          </section>
         </section>
       </MaxWidthWrapper>
     </div>
