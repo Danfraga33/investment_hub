4dec87ae4ab94494e8a73eb3d564b7ee793704f6
Daniel Fraga <dan.fraga33@gmail.com>
2024-05-30 21:26:30 +0100

    chore: remote unneeded middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 import { auth } from "@/lib/auth";
 
 export async function middleware(request: NextRequest) {
-  const session = await auth();
+  // const session = await auth();
   // if (!session) {
   //   return NextResponse.redirect(new URL("/api/auth/signin", request.url));
   // }
