3120d2c7fe969fae72a8246611a0bd11a42f923f
Daniel Fraga <dan.fraga33@gmail.com>
2024-05-30 21:26:10 +0100

    feat: layout comp for dyanmic lnks

---
+++ src/app/dashboard/[portfolioId]/[symbol]/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import { Sidebar } from "../../../components/DashboardComponents/Sidebar";
+import { auth } from "@/lib/auth";
+
+export default async function DashboardLayout({
+  children, // will be a page or nested layout
+}: {
+  children: ReactNode;
+}) {
+  const session = await auth();
+  return (
+    <section>
+      {session && <Sidebar />}
+      {children}
+    </section>
+  );
+}
