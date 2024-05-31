6b1d55ed2fa8fb6e1918c42ee4ba5963963e8886
Daniel Fraga <dan.fraga33@gmail.com>
2024-05-31 22:06:37 +0100

    chore: move files to dashboard component

--- src/app/dashboard/PDFChat/page.tsx
+++
@@ -1,6 +0,0 @@
-import PDFChatPage from "@/app/components/DashboardComponents/DashboardPages/PDFChat";
-const PDFChat = () => {
-  return <PDFChatPage />;
-};
-
-export default PDFChat;
--- src/app/dashboard/[portfolioId]/[stockId]/layout.tsx
+++ src/app/dashboard/[portfolioId]/[stockId]/layout.tsx
@@ -3,14 +3,19 @@ import { Sidebar } from "../../../components/DashboardComponents/Sidebar";
 import { auth } from "@/lib/auth";
 
 export default async function DashboardLayout({
-  children, // will be a page or nested layout
+  children,
+  params, // will be a page or nested layout
 }: {
   children: ReactNode;
+  params: { portfolioId: string; stockId: string };
 }) {
   const session = await auth();
+
   return (
     <section>
-      {session && <Sidebar />}
+      {session && (
+        <Sidebar portfolioId={params.portfolioId} stockId={params.stockId} />
+      )}
       {children}
     </section>
   );
