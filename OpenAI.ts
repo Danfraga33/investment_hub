e6211eeb58a86345373d568983804c4b1a10de7d
Daniel Fraga <dan.fraga33@gmail.com>
2024-06-01 15:05:40 +0100

    feat: change from langchain to vercel ai

--- OpenAI.ts
+++ OpenAI.ts
@@ -22,7 +22,7 @@
 //     openAIApiKey: "sk-proj-JSbitKpfqOYwPWtlbYuDT3BlbkFJXswtKWvDhBPP5enwlHBk",
 //   });
 //   const prompt = ChatPromptTemplate.fromMessages([
-//     [ 
+//     [
 //       "human",
 //       "Summarize {topic} as someone providing insights to the following data, using an opportunistic and positive tone.Extract 5 key headings and give 75-125 word summary per heading. Return your response in this markdown.",
 //     ],
@@ -30,9 +30,24 @@
 //   const promptValue = await prompt.invoke({ topic: text });
 //   const promptAsMessages = promptValue.toChatMessages();
 //   const response = await model.invoke(promptAsMessages);
+//   console.log(response);
 //   return response;
 //   // const chain = promptTemplate.pipe(model);
 
 //   // const result = await chain.invoke({ topic: text });
 //   // return result.lc_kwargs.content;
 // }
+
+import { openai } from "@ai-sdk/openai";
+import { generateObject, generateText, streamObject, streamText } from "ai";
+import { z } from "zod";
+require("dotenv").config();
+
+export async function ChatSum(docs: any) {
+  const result = await generateText({
+    model: openai("gpt-3.5-turbo"),
+    prompt: `Summarize ${docs} as someone providing insights.`,
+  });
+
+  console.log(result.text);
+}
