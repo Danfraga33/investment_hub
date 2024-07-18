f29ce4ea8f1b1e152186d799197b145b14e3e0cc
Daniel Fraga <dan.fraga33@gmail.com>
2024-07-18 08:13:37 +0100

    fix: remote unneccessary async awaits

--- src/app/dashboard/[portfolioId]/[stockId]/Analytics/page.tsx
+++ src/app/dashboard/[portfolioId]/[stockId]/Analytics/page.tsx
@@ -1,7 +1,5 @@
 import AnalyticsPage from "@/app/components/DashboardComponents/DashboardPages/Analytics";
-import { getPortfolio } from "@/app/components/DashboardData";
 const Analytics = async () => {
-  const res = await getPortfolio();
   return <AnalyticsPage />;
 };
 
